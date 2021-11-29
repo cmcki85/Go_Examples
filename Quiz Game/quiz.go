4a5ffd808e2e8d465a4216edb189f2e54685892a
Cameron Mckiddie <cameronsmowing@gmail.com>
2021-11-29 17:22:46 +1000

    Completed Quiz Game Started Docker Tutorials

--- Quiz Game/quiz.go
+++ Quiz Game/quiz.go
@@ -2,44 +2,57 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-func readCSV(filepath string) [][]string {
-	fp, err := os.Open(filepath)
+func formatProblems(data [][]string, timeLimit int) {
+	var solution string
+	var scoredPoints int
+	totalPoints := len(data)
+
+	gameTimer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
+	for i := range data {
+		fmt.Println("What is " + data[i][0] + "?")
+		answerChannel := make(chan string)
+		go func() {
+			fmt.Scan(&solution)
+			answerChannel <- solution
+		}()
+
+		select {
+		case <-gameTimer.C:
+			fmt.Printf("\nYou scored: %d, out of %d!", scoredPoints, totalPoints)
+			return
+		case solution := <-answerChannel:
+			if solution == data[i][1] {
+				scoredPoints += 1
+			}
+		}
+	}
+	fmt.Printf("You scored: %d, out of %d!", scoredPoints, totalPoints)
+}
+
+func main() {
+	fileName := flag.String("fp", "./problems.csv", "The default filepath to parsable CSV.")
+	timeLimit := flag.Int("limit", 30, "The default time limit of the game. (seconds)")
+	flag.Parse()
+
+	fp, err := os.Open(*fileName)
 	if err != nil {
-		log.Fatal("Unable to read file path "+filepath, err)
+		log.Fatal("Unable to read file path "+*fileName, err)
 	}
 	defer fp.Close()
 
 	csvReader := csv.NewReader(fp)
 	data, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal("Unable to parse CSV file for "+filepath, err)
+		log.Fatal("Unable to parse CSV file for "+*fileName, err)
 	}
 
-	return data
-}
-
-func formatProblems(data [][]string) {
-	var solution string
-	var scoredPoints int
-
-	for i := range data {
-		fmt.Println("What is " + data[i][0] + "?")
-
-		fmt.Scan(&solution)
-		if solution == data[i][1] {
-			scoredPoints += 1
-		}
-	}
-	totalPoints := len(data)
-	fmt.Printf("You scored: %d, out of %d!", scoredPoints, totalPoints)
-}
-
-func main() {
-	data := readCSV("./problems.csv")
-	formatProblems(data)
+	formatProblems(data, *timeLimit)
 }
